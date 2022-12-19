33917bece70422b70ea3ec42f55f9a69cc419161
LamboLead <juandlopez01@hotmail.com>
2022-12-19 13:30:48 -0500

    Moved table code to dev-table-searchbar branch

--- src/views/test/searchMethods/comparison.js
+++
@@ -1,5 +0,0 @@
-const greaterThan = (array, fieldName, comparator, standardize) => {
-  return filterArray(array, fieldName, standardize, (value) => {
-    return value > comparator;
-  });
-}
--- src/views/test/searchMethods/equals.js
+++
@@ -1,5 +0,0 @@
-const equals = (array, fieldName, searchString, standardize) => {
-  return filterArray(array, fieldName, standardize, (value) => {
-    return value === searchString;
-  });
-}
