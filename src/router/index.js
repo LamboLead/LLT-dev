bf215dd7e7ded3cf2e5b4ae53d6d0fa878199c2f
LamboLead <juandlopez01@hotmail.com>
2022-12-19 13:52:59 -0500

    Created form branch

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from '/src/views/1-1_main-page/MainPage.vue';
-import TableView from '/src/views/1-2_table/Table.vue';
-// import Semillero from '/src/components/views/1-3_semillero/Semillero.vue';
-// import Repository from '/src/components/views/1-4_repository/Repository.vue';
-// import Events from '/src/components/views/1-5_events/Events.vue';
-// import Vinculation from '/src/components/views/1-6_vinculation/Vinculation.vue';
-// import Member from '/src/components/views/1-7_member/Member.vue';
-// import ProjectRequest from '/src/components/views/1-8_project-request/ProjectRequest.vue';
-
-// import LogIn from '/src/components/views/2-1_login/LogIn.vue';
-// import AdminSolicitudes from '/src/components/views/2-2_admin-solicitudes/AdminSolicitudes.vue';
-
-import Test from '/src/views/test/Test.vue';
+import FormView from '/src/views/form/FormPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL), // Allows the user to go back and forth in browsing
@@ -22,64 +11,10 @@ const router = createRouter({
       component: MainPage
     },
     {
-      path: '/table',
-      name: 'table',
-      component: TableView
-    },
-    // {
-    //   path: '/repositorio',
-    //   name: 'repositorio',
-    //   component: Repository
-    // },
-    // {
-    //   path: '/eventos',
-    //   name: 'eventos',
-    //   component: Events
-    // },
-    // {
-    //   path: '/el-semillero',
-    //   name: 'semillero',
-    //   component: Semillero
-    // },
-    // {
-    //   path: '/vinculate',
-    //   name: 'vinculation',
-    //   component: Vinculation
-    // },
-    // {
-    //   path: '/miembros',
-    //   name: 'member',
-    //   component: Member
-    // },
-    // {
-    //   path: '/solicitudes',
-    //   name: 'request',
-    //   component: ProjectRequest
-    // },
-    // {
-    //   path: '/iniciar-sesion',
-    //   name: 'login',
-    //   component: LogIn
-    // },
-    // {
-    //   path: '/admin/solicitudes',
-    //   name: 'admin-solicitudes',
-    //   component: AdminSolicitudes
-    // },
-    {
-      path: '/test',
-      name: 'test',
-      component: Test
+      path: '/form',
+      name: 'form',
+      component: FormView
     }
-
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
   ]
 })
 
---
+++ src/views/form/FormPage.vue
@@ -0,0 +1,38 @@
+<script>
+
+// Import components
+import ContactForm from '/LLT/frontend/components/form/Form.vue';
+
+// Import config
+import formConfig from './components/contact-form/config.js';
+
+export default {
+  setup() {
+    return {
+      formConfig: formConfig
+    }
+  },
+  components: {
+    ContactForm
+  }
+}
+</script>
+
+<template>
+  <section>
+    <ContactForm
+      :config="this.formConfig"
+    />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  position: relative;
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
